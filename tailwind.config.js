ad93db08540a808e772fdcdde5b1b2d3bb33199b
Ruben Smidt <ruben@smidt-bremen.de>
2021-06-30 15:27:50 +0200

    feat: increase 'My stack' spacing and logo overflow

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,8 @@ const overrideContainer = ({ addComponents }) => {
       width: '100%',
       marginLeft: 'auto',
       marginRight: 'auto',
+      paddingLeft: '1rem',
+      paddingRight: '1rem',
       '@screen sm': {
         maxWidth: '640px',
       },
